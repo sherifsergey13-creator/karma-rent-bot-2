c52471f08065cbeb7b18d76b8d5cf1e48ef24b06
sherifsergey13-creator <sherif.sergey13@gmail.com>
2026-05-21 00:26:58 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,3 +36,18 @@ def reply(message):
 
 print("Бот запущен...")
 bot.infinity_polling()
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
